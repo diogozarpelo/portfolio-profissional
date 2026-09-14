f9f5cafe445124ccc471e007f1987be883c7efb6
diogozarpelo <diogozarpelao@gmail.com>
2026-09-14 14:50:40 -0300

    perf: limit font loading to Latin subsets

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 ﻿import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import '@fontsource-variable/manrope'
-import '@fontsource-variable/jetbrains-mono'
+import './fonts.css'
 import './index.css'
 import App from './App.tsx'
 
